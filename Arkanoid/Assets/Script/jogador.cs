4acec5c462fcec3b531de18f4cb7cfdcbcb241c0
LEONARDO AUGUSTO DO PRADO <leonardo.aprado@senacsp.edu.br>
2019-04-24 22:22:53 -0300

    Criação do jogador e seu script

---
+++ Arkanoid/Assets/Script/jogador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jogador : MonoBehaviour
+{
+    public float velocidade = 50.0f;
+
+    public float horizontalAxis;
+
+
+    
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalAxis * velocidade, 0);
+    }
+}
